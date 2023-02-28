06caab30e36110c83ac5423a491422361ef01566
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-02-28 12:10:35 -0500

    Add files via upload

---
+++ youtube_downloader.py
@@ -0,0 +1,13 @@
+From pytube import YouTube
+From sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("View: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.downloand('directory to download folder')
